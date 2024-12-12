4349ea798f686a2e41baff94bfe1572c7dc00326
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-12-12 11:30:51 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class Config
 {
@@ -19,10 +19,26 @@ class Config
         $this->connect();
     }
 
-    public function insertData($name, $age, $course, $contact)
+    public function insertData($name, $age, $contact, $course)
     {
         $query = "INSERT INTO students (name, age, contact, course) VALUES ('$name', '$age', '$contact', '$course')";
         mysqli_query($this->connection, $query);
+    }
+
+    public function readData(){
+        $query = "SELECT * FROM $this->tableName";
+        $res = mysqli_query($this->connection, $query);
+        return $res;
+    }
+
+    public function updateData($id, $name, $age, $contact, $course){
+        $query = "UPDATE $this->tableName SET name = '$name', age = $age, contact = $contact, course = '$course' WHERE id = $id";
+        return $res = mysqli_query($this->connection, $query);
+    }
+
+    public function deletedata($id){
+        $query = "DELETE FROM $this->tableName WHERE id = $id";
+        return $res = mysqli_query($this->connection, $query);
     }
 }
 
